9144be85736565c780ec777dcebbc7d447866b14
mastercad <andreas.kempe@byte-artist.de>
2019-07-21 21:47:41 +0200

    testcommit

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -74,7 +74,7 @@ class ClientController extends AbstractController
      * List all Available Clients in Network
      *
      * @param NetworkScanner $networkScanner Service for scanning network.
-     * 
+     *
      * @Route("/clients/online", name="clients-online-index")
      * 
      * @return JsonResponse
